241b7e019ad480f95fe8ba2325c19d60aba50273
jayti20 <jaytimatrix@gmail.com>
2023-09-26 20:27:18 +0530

    MVC pattern and manipulating array of objects using templating engine

---
+++ mvc.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const handlebars = require("express-handlebars").create({
+  defaultLayout: "main",
+});
+app.engine("handlebars", handlebars.engine);
+app.set("view engine", "handlebars");
+const userlist = [
+  { id: 1, username: "Samuel" },
+  { id: 2, username: "Clark" },
+  { id: 3, username: "Luke" },
+  { id: 4, username: "Lorelai" },
+  { id: 5, username: "Alex" },
+];
+app.get("/userlist", (req, res) => {
+  res.render("user", { userlist });
+});
+
+app.listen(8000);
